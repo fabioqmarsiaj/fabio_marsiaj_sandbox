4d94d1b2edd866812dff4e05cdd8889dd42c36f2
Fabio <fabiomarsiaj@gmail.com>
2020-10-10 21:47:40 -0300

    project setup

---
+++ spring_webflux/src/test/java/com/fabiomarsiaj/spring_webflux/SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiomarsiaj.spring_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
