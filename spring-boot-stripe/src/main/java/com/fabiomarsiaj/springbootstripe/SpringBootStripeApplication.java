7619d86b6af025af1b330cda68058e109f863e02
Fabio <fabiomarsiaj@gmail.com>
2020-10-29 08:55:10 -0300

    stripe init

---
+++ spring-boot-stripe/src/main/java/com/fabiomarsiaj/springbootstripe/SpringBootStripeApplication.java
@@ -0,0 +1,13 @@
+package com.fabiomarsiaj.springbootstripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStripeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStripeApplication.class, args);
+	}
+
+}
