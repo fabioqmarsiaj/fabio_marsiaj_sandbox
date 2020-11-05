182e777a84f90fd63f18c7a74922a6e432798e69
Fabio <fabiomarsiaj@gmail.com>
2020-11-05 12:08:17 -0300

    Now supporting the PaymentIntent API, newer version

--- spring-boot-stripe/src/main/java/com/fabiomarsiaj/springbootstripe/controller/PaymentController.java
+++ spring-boot-stripe/src/main/java/com/fabiomarsiaj/springbootstripe/controller/PaymentController.java
@@ -4,7 +4,7 @@ import com.fabiomarsiaj.springbootstripe.domain.Currency;
 import com.fabiomarsiaj.springbootstripe.domain.PaymentRequest;
 import com.fabiomarsiaj.springbootstripe.service.StripeService;
 import com.stripe.exception.StripeException;
-import com.stripe.model.Charge;
+import com.stripe.model.PaymentIntent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,12 +23,12 @@ public class PaymentController {
         paymentRequest.setDescription("Example charge");
         paymentRequest.setCurrency(Currency.EUR);
 
-        Charge charge = paymentService.charge(paymentRequest);
+        PaymentIntent paymentIntent = paymentService.charge(paymentRequest);
 
-        model.addAttribute("id", charge.getId());
-        model.addAttribute("status", charge.getStatus());
-        model.addAttribute("chargeId", charge.getId());
-        model.addAttribute("amount_received", charge.getBalanceTransaction());
+        model.addAttribute("id", paymentIntent.getId());
+        model.addAttribute("status", paymentIntent.getStatus());
+        model.addAttribute("chargeId", paymentIntent.getId());
+        model.addAttribute("amount_received", paymentIntent.getAmountReceived());
         return "result";
     }
 
--- spring-boot-stripe/src/main/java/com/fabiomarsiaj/springbootstripe/service/StripeService.java
+++ spring-boot-stripe/src/main/java/com/fabiomarsiaj/springbootstripe/service/StripeService.java
@@ -3,9 +3,10 @@ package com.fabiomarsiaj.springbootstripe.service;
 import com.fabiomarsiaj.springbootstripe.domain.PaymentRequest;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
-import com.stripe.model.Charge;
 import com.stripe.model.PaymentIntent;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,13 +23,16 @@ public class StripeService {
         Stripe.apiKey = secretKey;
     }
 
-    public Charge charge(PaymentRequest paymentRequest)
+    public PaymentIntent charge(PaymentRequest paymentRequest)
             throws StripeException {
+        List<Object> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
         Map<String, Object> params = new HashMap<>();
         params.put("amount", paymentRequest.getAmount());
         params.put("currency", paymentRequest.getCurrency());
         params.put("description", paymentRequest.getDescription());
-        params.put("source", paymentRequest.getStripeToken());
-        return Charge.create(params);
+        params.put("payment_method_types", paymentMethodTypes);
+        return PaymentIntent.create(params);
     }
 }
