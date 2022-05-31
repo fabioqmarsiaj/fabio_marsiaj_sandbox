5dce297fbc8c0ff3aff8fee3d1b3582b02d815ab
Fabio Marsiaj <cwi.fmarsiaj@bv.com.br>
2022-05-31 12:16:15 -0300

    refactor nas pocs ja existentes

--- netflix_ribbon_poc/src/main/java/com/fabioqmarsiaj/ribbon/RibbonLoadBalance.java
+++ netflix_ribbon_poc/src/main/java/com/fabioqmarsiaj/ribbon/RibbonLoadBalance.java
@@ -3,6 +3,7 @@ package com.fabioqmarsiaj.ribbon;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
+
 import com.google.common.collect.Lists;
 import com.netflix.loadbalancer.BaseLoadBalancer;
 import com.netflix.loadbalancer.ILoadBalancer;
@@ -15,20 +16,20 @@ import rx.Observable;
 
 
 public class RibbonLoadBalance {
-	
-	private final ILoadBalancer loadBalancer;
-	
-	public RibbonLoadBalance(List<Server> servers) {
-		super();
-		this.loadBalancer = LoadBalancerBuilder.newBuilder().buildFixedServerListLoadBalancer(servers);
-	}
-	
-	private String call(final String path) {
-	        return LoadBalancerCommand.<String>builder()
-	                .withLoadBalancer(loadBalancer)
-	                .build()
-	                .submit(
-                		new ServerOperation<String>() {
+
+    private final ILoadBalancer loadBalancer;
+
+    public RibbonLoadBalance(List<Server> servers) {
+        super();
+        this.loadBalancer = LoadBalancerBuilder.newBuilder().buildFixedServerListLoadBalancer(servers);
+    }
+
+    private String call(final String path) {
+        return LoadBalancerCommand.<String>builder()
+                .withLoadBalancer(loadBalancer)
+                .build()
+                .submit(
+                        new ServerOperation<String>() {
                             @Override
                             public Observable<String> call(Server server) {
                                 URL url;
@@ -41,31 +42,22 @@ public class RibbonLoadBalance {
                                 }
                             }
                         })
-	                .toBlocking()
-	                .first();
+                .toBlocking()
+                .first();
     }
-	
-	public LoadBalancerStats getLoadBalancerStats() {
-        return ((BaseLoadBalancer) loadBalancer).getLoadBalancerStats();
+
+    public static void main(String[] args) {
+
+        RibbonLoadBalance urlLoadBalancer = new RibbonLoadBalance(Lists.newArrayList(
+                new Server("www.google.com", 80),
+                new Server("www.linkedin.com", 80),
+                new Server("www.yahoo.com", 80)));
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println(urlLoadBalancer.call("/"));
+        }
+
     }
-	
-	
-	public static void main(String[] args) {
-		
-		RibbonLoadBalance urlLoadBalancer = new RibbonLoadBalance(Lists.newArrayList(
-	                new Server("www.google.com", 80),
-	                new Server("www.linkedin.com", 80),
-	                new Server("www.yahoo.com", 80)));
-	
-		for (int i = 0; i < 6; i++) {
-		        System.out.println(urlLoadBalancer.call("/"));
-		}
-		
-		System.out.println("======== Load balancer stats ========");
-
-        System.out.println(urlLoadBalancer.getLoadBalancerStats());
-
-	}
 
 }
 
