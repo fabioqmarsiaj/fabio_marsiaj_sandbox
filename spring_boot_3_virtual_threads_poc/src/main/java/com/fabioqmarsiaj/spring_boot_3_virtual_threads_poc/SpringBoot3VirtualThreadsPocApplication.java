f9b746fc85088ba64079a1711d274b37afe43160
Fabio Marsiaj <fabiobackend@gmai.com>
2023-12-17 21:37:03 -0300

    spring boot 3 virtual threads poc init

---
+++ spring_boot_3_virtual_threads_poc/src/main/java/com/fabioqmarsiaj/spring_boot_3_virtual_threads_poc/SpringBoot3VirtualThreadsPocApplication.java
@@ -0,0 +1,13 @@
+package com.fabioqmarsiaj.spring_boot_3_virtual_threads_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3VirtualThreadsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3VirtualThreadsPocApplication.class, args);
+	}
+
+}
---
+++ spring_boot_3_virtual_threads_poc/src/test/java/com/fabioqmarsiaj/spring_boot_3_virtual_threads_poc/SpringBoot3VirtualThreadsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabioqmarsiaj.spring_boot_3_virtual_threads_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3VirtualThreadsPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
