f1caa57e3f082e34441a4a45f36c089a2c7e0fbe
Fabio Marsiaj <fabiobackend@gmai.com>
2023-12-17 21:50:17 -0300

    Creating 100.000 traditional threads: 13186 ms

--- spring_boot_3_virtual_threads_poc/src/main/java/com/fabioqmarsiaj/spring_boot_3_virtual_threads_poc/SpringBoot3VirtualThreadsPocApplication.java
+++ spring_boot_3_virtual_threads_poc/src/main/java/com/fabioqmarsiaj/spring_boot_3_virtual_threads_poc/SpringBoot3VirtualThreadsPocApplication.java
@@ -1,13 +1,37 @@
 package com.fabioqmarsiaj.spring_boot_3_virtual_threads_poc;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+import java.time.Instant;
 
 @SpringBootApplication
 public class SpringBoot3VirtualThreadsPocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot3VirtualThreadsPocApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3VirtualThreadsPocApplication.class, args);
+    }
 
+	@Bean
+    public ApplicationRunner runner() {
+        return args -> {
+            var startDate = Instant.now();
+
+            startTraditionalThreads();
+            var finishDate = Instant.now();
+            System.out.printf("Duration Time(Milliseconds): %s%n", Duration.between(startDate, finishDate).toMillis());
+        };
+    }
+
+    private void startTraditionalThreads() throws InterruptedException {
+        for (int i = 0; i < 100_000; i++) {
+            int finalI = i;
+            Thread t = new Thread(() -> System.out.println(finalI));
+            t.start();
+            t.join();
+        }
+    }
 }
